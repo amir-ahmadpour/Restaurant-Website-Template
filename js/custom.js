4aac7337fae6a2b13ad9bbb8b7ac63c0dffb7b61
Amir-Ah <amirah3624@gmail.com>
2021-04-23 15:14:54 +0430

    change btn navbar menu by JQuery

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,18 @@
 // Gallery BaguetteBox script
- baguetteBox.run('.gallery');
+baguetteBox.run(".gallery");
+
+// Btn Navbar Menu
+$(document).ready(function () {
+  
+    let cheched = false;
+  
+  $(".menu-btn").on("click", function () {
+    if (cheched) {
+      $(".logo-img").css("margin", "");
+      cheched = false;
+    } else {
+      $(".logo-img").css("margin", "auto");
+      cheched = true;
+    }
+  });
+});
